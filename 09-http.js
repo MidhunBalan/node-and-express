c86bf4394cd9bc546944eed8edb6b2a664974639
midhunbalan <midhun.balan@anywhere.co>
2024-01-27 19:39:13 +0530

    feat: event loop

---
+++ 09-http.js
@@ -0,0 +1,11 @@
+//npm ->  global command comes with node
+//npm --version -> to check the version of npm
+
+//You can install the dependency as local and global.
+
+//local dependency -> use it only in this particular project
+//npm i <package-name> -> to install a package
+
+//global dependency -> use it in any project
+//npm install -g <packageName> -> to install any package as global
+//sudo install -g <packageName> (mac)
